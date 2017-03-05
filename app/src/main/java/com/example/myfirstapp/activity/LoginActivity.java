19dac21eaefc5410ff5aabc9dfcfd24411a39980
MFranco <maykoolfr@gmail.com>
2017-03-05 16:52:05 -0600

    Login, mensajes y dialogo de progreso

--- app/src/main/java/com/example/myfirstapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/activity/LoginActivity.java
@@ -1,24 +1,24 @@
 package com.example.myfirstapp.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.myfirstapp.ListaMiembroActivity;
+import com.example.myfirstapp.ControlGymApplication;
 import com.example.myfirstapp.PlanNutrional;
 import com.example.myfirstapp.R;
-import com.example.myfirstapp.adapter.MiembrosAdapter;
 import com.example.myfirstapp.model.Miembro;
 import com.example.myfirstapp.rest.ApiClient;
 import com.example.myfirstapp.rest.ApiInterface;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
@@ -26,45 +26,65 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = ListaMiembroActivity.class.getSimpleName();
-    Button Loginn;
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private final Context thisContext = this;
+    Button Login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Loginn=(Button) findViewById(R.id.btn_login);
-        Loginn.setOnClickListener(new View.OnClickListener() {
+        Login =(Button) findViewById(R.id.btn_login);
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enviarDatosLogin("jacklin@gmail.com", "clave");
-                Intent Loginn=new Intent(LoginActivity.this, PlanNutrional.class);
-                startActivity(Loginn);
-
+                EditText texto = (EditText) findViewById(R.id.correoLogin);
+                String correo = texto.getText().toString();
+                texto = (EditText) findViewById(R.id.contrasenaLogin);
+                String clave = texto.getText().toString();
+                enviarDatosLogin(correo, clave);
+                // enviarDatosLogin("jacklin@gmail.com", "clave");
             }
         });
     }
 
-    private void enviarDatosLogin(String correo, String password){
+    private void enviarDatosLogin(String correo, String clave){
+        // Dialogo de carga para hacer saber al usuario que hay un proceso
+        final ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("Autenticando usuario");
+        progress.setMessage("Por favor espere un momento...");
+        progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+        progress.show(); // ejecutar progress.dismiss(); para detener el dialogo de carga
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        // Generando parametros json
+        // Generando parametros para luego ser parseados a formato json
         Map<String, String> params = new HashMap<>();
         params.put("Correo", correo);
-        params.put("Clave", password);
+        params.put("Clave", clave);
         //Llamando metodo POST con el objecto correspondiente como parametro
         Call<Miembro> call = apiService.autenticar(params);
         call.enqueue(new Callback<Miembro>() {
             @Override
             public void onResponse(Call<Miembro>call, Response<Miembro> response) {
-                Log.e(TAG, "response.body().getResults(): " + response.body().getToken());
-                Miembro miembro = response.body();
+                if (response.body().getToken() != null
+                        && !response.body().getToken().isEmpty()){
+                    Log.d(TAG, "response.body().getResults(): " + response.body().getToken());
+                    Miembro miembro = response.body();
 
+                    Intent Loginn=new Intent(LoginActivity.this, PlanNutrional.class);
+                    startActivity(Loginn);
+                } else {
+                    Log.d(TAG, "Toast message: " + "Correo o Clave incorrectos");
+                    Toast.makeText(ControlGymApplication.getContext(), "Correo o Clave incorrectos", Toast.LENGTH_SHORT).show();
+                }
+                progress.dismiss();
             }
 
             @Override
             public void onFailure(Call<Miembro>call, Throwable t) {
                 // Log error here since request failed
+                progress.dismiss();
                 Log.e(TAG, t.toString());
             }
         });
